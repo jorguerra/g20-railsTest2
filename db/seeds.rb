9dd824204e35c88d8368671833c47b144e406bbe
Jorge Guerra <jorguerra@gmail.com>
2019-08-31 13:02:54 -0400

    Seeder con Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do |i|
+  food = Faker::Food.dish
+  Todo.create(
+    title: "Eat #{food}",
+    photo: "http://lorempixel.com/400/200/food/#{i}"
+  )
+end
