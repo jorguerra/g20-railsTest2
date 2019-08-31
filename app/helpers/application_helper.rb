fe3d74b14a64805c0451ea24758e850e59edb858
Jorge Guerra <jorguerra@gmail.com>
2019-08-31 12:09:40 -0400

    Fix for logout link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   def navbar_session
     text = user_signed_in? ? 'Logout' : 'Login'
     link = user_signed_in? ?  destroy_user_session_path : new_user_session_path
-    link_to text, link, class:'nav-link'
+    method = user_signed_in? ? 'DELETE' : 'GET'
+    link_to text, link, class:'nav-link', method: method
   end
 end
