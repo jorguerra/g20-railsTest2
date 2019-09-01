60b3e60f5e125fbb52080d0fcd0a3ed2274e66fe
Jorge Guerra <jorguerra@gmail.com>
2019-09-01 11:27:44 -0400

    Listing todos

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,10 @@
 module TodosHelper
+
+  def showTodoBoton(todo)
+    unless current_user.has_todo?(todo)
+     link_to 'No listo', completed_path(todo: todo), method: :post, class: 'btn btn-primary'
+    else
+     link_to 'Listo', uncomplete_path(todo: todo), method: :delete, class: 'btn btn-danger'
+    end
+  end
 end
