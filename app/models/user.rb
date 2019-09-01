84cd8143ef6d8466b20ae0e5adc415658fd8e96a
Jorge Guerra <jorguerra@gmail.com>
2019-09-01 11:12:27 -0400

    Rerencing user's todos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :progress
+  has_many :todos, through: :progress
+
+  def has_todo?(todo)
+   self.todos.each do |t|
+     return true if t == todo
+   end
+   false
+  end
 end
